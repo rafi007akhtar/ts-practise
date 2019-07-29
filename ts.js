826a2912cb2dd180d67af1c2bb7d0b1721de31ee
rafi007akhtar <alimdrafi@gmail.com>
2019-07-29 19:25:32 +0000

    converted JS file

--- ts.js
+++ ts.js
@@ -12,9 +12,16 @@ var myEmp = {
 };
 EmpArr(myEmp);
 console.log(employees);
-var form = document.querySelector("form");
-form.addEventListener("submit", function () {
-    alert("submitted");
+var form = document.querySelector("#submit");
+form.addEventListener("click", function () {
+    var firstname = document.querySelector("#firstname").value;
+    var lastname = document.querySelector("#lastname").value;
+    var empid = document.querySelector("#empid").value;
+    var phone = document.querySelector("#phone").value;
+    var address = document.querySelector("#address").value;
+    var email = document.querySelector("#email").value;
+    var newEmp = { firstname: firstname, lastname: lastname, empid: empid, phone: phone, address: address, email: email };
+    EmpArr(newEmp);
+    alert("New Record added");
+    console.log(employees);
 });
-var firstname = document.querySelector("#firstname").value;
-console.log(firstname);
